4f8e09892ce7900f9c7180bb1dcd328a1e266cbe
jereverd <jereverd@Inception-of-things.myguest.virtualbox.org>
2024-08-29 17:11:01 +0200

    Instal and uninstal script + makefile

---
+++ p3/instal-script.sh
@@ -0,0 +1,23 @@
+#install docker
+sudo apt update -q
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+#install K3D
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+sudo k3d cluster create mycluster 
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv ./kubectl /usr/local/bin
+
+# Create namespaces
+sudo kubectl create namespace argocd
+sudo kubectl create namespace dev
+
+# installing 
+sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sudo kubectl apply -n argocd -f /home/jereverd/Documents/Inception_of_things/p3/ingress.yaml 
+sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > argocd_password.txt
+# sudo kubectl port-forward svc/argocd-server -n argocd 8080:443
