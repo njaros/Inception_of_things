052763a7542a7764cb2d5a283e00eca74852a69e
Iot <iot@Iot.myguest.virtualbox.org>
2024-09-03 17:11:03 +0200

    bonus

---
+++ bonus/scripts/install_all.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# General Install
+echo "----------Updating Binary--------"
+sudo apt-get update
+
+# Install Docker
+echo "-----------Installing Docker---------"
+sudo curl -fsSL https://get.docker.com | sh -
+echo "-----------Enabling Docker And ContainerD Service---------"
+sudo systemctl enable --now docker.service
+sudo systemctl enable --now containerd.service
+sudo systemctl start docker
+echo "-----------Docker Should Be Ready To Use----------"
+
+# Install K3D
+echo "-----------Installing K3D-----------"
+sudo curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | sudo bash
+echo "-----------Creating K3D Cluster-----------"
+sudo k3d cluster create -p "8080:80" jrinnacluster
+
+# Install Kubectl
+echo "-----------Installing Kubectl-----------"
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo chmod +x kubectl
+echo "-----------Moving Kubectl------------"
+sudo mv ./kubectl /usr/local/bin
+
+# Creating namespace
+echo "----------Creating NameSpace------------"
+sudo kubectl create namespace argocd
+sudo kubectl create namespace dev
+
+# Install ArgoCD in k3d cluster
+echo "----------Adding ArgoCD To The K3D Cluster----------"
+sudo kubectl apply -n argocd -f ./confs/argocd.yaml # we used this because we've modify the installation script a bit on line 10270 ish
+echo "----------Waitting For Pods"
+sleep 10
+sudo kubectl wait -n argocd --for=condition=Ready pods --all --timeout=120s
+echo "----------Port Forwarding-----------"
+sudo kubectl apply -f ./confs/ingress.yaml
+sudo kubectl apply -n argocd -f ./confs/application.yaml
+sudo kubectl get secret argocd-initial-admin-secret -n argocd -o yaml | grep password | awk '{print $2}' | base64 -d > password.txt
+
+# Installing gitlab
+sudo curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# Testing
+echo -n "---------Testing install---------"
+sudo docker --version
+sudo k3d --version
+sudo kubectl version
+sudo kubectl get all --all-namespaces
---
+++ bonus/scripts/uninstall_all.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+sudo rm -rf password.txt
+
+# ---------------------------------------------------------------------------- #
+#                                Helm Uninstall                                #
+# ---------------------------------------------------------------------------- #
+
+# Capture the real user's home directory
+REAL_HOME=$(eval echo ~${SUDO_USER})
+
+# Remove the helm binary
+sudo rm -rf /usr/local/bin/helm
+
+# Remove Helm cache
+sudo rm -rf "$REAL_HOME/.cache/helm"
+
+# Remove Helm configuration
+sudo rm -rf "$REAL_HOME/.config/helm"
+
+# Remove Helm data
+sudo rm -rf "$REAL_HOME/.local/share/helm"
+
+# Remove any Helm plugins (if applicable)
+sudo rm -rf "$REAL_HOME/.helm"
+
+# ---------------------------------------------------------------------------- #
+#                              K3D Uninstallation                              #
+# ---------------------------------------------------------------------------- #
+
+# Delete all k3d clusters
+echo "Deleting all k3d clusters..."
+sudo k3d cluster delete --all
+
+# Remove the k3d binary
+echo "Removing k3d binary..."
+sudo rm $(which k3d)
+
+# Remove k3d configuration files
+echo "Removing k3d configuration files..."
+sudo rm -rf ~/.k3d
+
+echo "k3d uninstallation and cleanup completed."
+
+# ---------------------------------------------------------------------------- #
+#                               Uninstall Docker                               #
+# ---------------------------------------------------------------------------- #
+
+# Stop all running containers
+sudo docker stop $(sudo docker ps -aq)
+
+# Remove all containers image volumes etc
+sudo docker system prune -af --volumes
+
+# Uninstall Docker packages
+sudo apt-get purge -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Remove Docker directories
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
+
+# Remove Docker GPG key and repository
+sudo rm /etc/apt/keyrings/docker.gpg
+sudo rm /etc/apt/sources.list.d/docker.list
+
+# Auto-remove any leftover dependencies
+sudo apt-get autoremove -y
+
+# Clean up package cache
+sudo apt-get clean
+
+# ---------------------------------------------------------------------------- #
+#                               Kubectl Uninstall                              #
+# ---------------------------------------------------------------------------- #
+
+# remove the binary
+sudo rm /usr/local/bin/kubectl
+
+# remove the config directory
+sudo rm -rf ~/.kube
+
+# remove alias
+sed -i '/alias kubectl/d' ~/.bashrc
+source ~/.bashrc
